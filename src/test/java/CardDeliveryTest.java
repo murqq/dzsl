5e5f99dae0cb745ba1c1504e9b5622c06dfbc2d9
Dmitriy.K <bomjnetop.Dr@gmail.com>
2023-07-30 23:04:31 +0300

    delete task 2

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -45,26 +45,4 @@ public class CardDeliveryTest {
         $("[data-test-id=notification].notification_visible .notification__content").shouldBe(visible).shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
 
     }
-
-    /*@Test
-    void sendFormUsingWidget(){
-
-        SelenideElement form = $(".form");
-        form.$("[data-test-id=city] input").setValue("Са");
-        $$(".menu .menu-item__control").find(exactText("Самара")).click();
-        form.$("[data-test-id=date] .icon-button").click();
-        if(!generateDate(3,"MM").equals(generateDate(7,"MM"))){
-            $(".calendar__title[data-step=1]").click();
-        }
-        String calendarDate = generateDate(7, "dd");
-        $$(".calendar__day").find(exactText(calendarDate)).click();
-        form.$("[data-test-id=name] input").setValue("Кривенцов Дмитрий");
-        form.$("[data-test-id=phone] input").setValue("+79649795766");
-        form.$("[data-test-id=agreement]").click();
-        form.$(".button").click();
-        $(".spin_visible").shouldBe(visible);
-        $("[data-test-id=notification].notification_visible .notification__title").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Успешно!"));
-        $("[data-test-id=notification].notification_visible .notification__content").shouldBe(visible).shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
-
-    } */
 }
